fa30f6e28a1df652cd1a9b63992f09a58388cf38
anhnt2808 <anh.nguyen@playnext-lab.co.jp>
2018-12-13 12:16:07 +0900

    Comment out force ssl. Set stage in deploy production.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
+set :stage, :production
 
 set :branch, "master"
 # role-based syntax
