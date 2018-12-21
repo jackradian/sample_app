9535f0fcae7d12f137da89ef2cada6cf913dc9ed
anhnt2808 <anh.nguyen@playnext-lab.co.jp>
2018-12-21 15:12:15 +0900

    Change deploy setting with rbenv.

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,13 @@ set :use_sudo, true
 set :linked_files, %w{.env config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
+
+set :rbenv_type, :user
 set :rbenv_ruby, '2.4.1'
+set :rbenv_path, '~/.rbenv'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all
 
 set :rails_env, fetch(:stage)
 # Bundler config
